bd1b7a529c9b3483c246cac26c912ad24ecaa913
ishimweerick <147871048+ishimweerick@users.noreply.github.com>
2024-08-02 18:41:42 +0200

    Update send-message.php

--- send-message.php
+++ send-message.php
@@ -21,37 +21,37 @@ if (isset($_POST["message"])) {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'kimjisooishart@gmail.com';
-        $mail->Password   = 'yrzibmtdsxcjqsgw';
+        $mail->Username   = 'clpaton955@gmail.com';
+        $mail->Password   = 'Code@4731#';
         $mail->SMTPSecure = 'ssl';
         $mail->Port       = 465;
 
         // Recipients
-        $mail->setFrom('kimjisooishart@gmail.com', 'Lorem Ipsum');
+        $mail->setFrom('clpaton955@gmail.com', 'Ubucuti Lodge');
         $mail->addAddress($_POST["email"]);
-        $mail->addReplyTo('kimjisooishart@gmail.com', 'Lorem Ipsum');
+        $mail->addReplyTo('clpaton955@gmail.com', 'Ubucuti Lodge');
 
         // Content
         $mail->isHTML(true);
         $mail->Subject = 'Thank You for Reaching Out!';
         $mail->Body = "
             <p>Dear $name,</p>
-            <p>Thank you for reaching out to us! We appreciate the time you took to share your feedback and inquiries with Lorem Ipsum Inc. Your input is valuable to us, and we are committed to providing you with the best possible experience.</p>
+            <p>Thank you for reaching out to us! We appreciate the time you took to share your feedback and inquiries with Ubucuti Lodge Inc. Your input is valuable to us, and we are committed to providing you with the best possible experience.</p>
 
             <p>Our team is currently reviewing your message, and we will get back to you as soon as possible. If your inquiry is time-sensitive, rest assured that we are working diligently to address it promptly.</p>
 
-            <p>In the meantime, feel free to explore our website for additional resources. If you have any urgent matters, please don't hesitate to contact us directly at (639) 234231.</p>
+            <p>In the meantime, feel free to explore our website for additional resources. If you have any urgent matters, please don't hesitate to contact us directly at +250793906570.</p>
 
-            <p>Once again, thank you for choosing Lorem Ipsum Inc.. We look forward to assisting you and appreciate your continued support.</p>
+            <p>Once again, thank you for choosing Ubucuti Lodge. We look forward to assisting you and appreciate your continued support.</p>
             <p>Best regards,</p>
-            <p>Lorem Ipsum</p>
+            <p>Ubucuti Lodge</p>
         ";
 
         $mail->send();
         echo "
             <script>
             alert('Message was sent successfully. Thank your for reaching us!');
-            document.location.href = 'index.php';
+            document.location.href = 'home';
             </script>
         ";
     } catch (Exception $e) {
