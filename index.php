e082be5464cb8d8c171e226ff57b4d3e0e0380fb
ishimweerick <147871048+ishimweerick@users.noreply.github.com>
2024-08-06 17:17:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -210,6 +210,12 @@ echo "No Slide found";
     </section>
    
     <!-- Testiominals -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js"></script>
+
+
+
 
 
     <div class="container-fluid bg-body-tertiary py-3">
