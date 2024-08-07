4530aef769a08811f8b99ce41969a7b903b8e801
ishimweerick <147871048+ishimweerick@users.noreply.github.com>
2024-08-07 08:37:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -504,7 +504,7 @@ while($row = mysqli_fetch_array($result)) {
 			<div class="single-testimonial">
 				<div class="round-1 round"></div>
 				<div class="round-2 round"></div>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+				<p><?php echo $row["message"]; ?><</p>
 				<div class="client-info">
 					<div class="client-video">
 						<a href="#"><img src="https://i.ibb.co/DWhSr6S/play-button2.png" alt=""></a>
