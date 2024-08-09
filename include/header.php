92cf174a8a865589d85dbc7627f36ccbb93e1198
ishimweerick <147871048+ishimweerick@users.noreply.github.com>
2024-08-09 10:49:39 +0200

    jazzsong_music.mp3

--- include/header.php
+++ include/header.php
@@ -92,6 +92,10 @@ while($row = mysqli_fetch_array($result))
     <link rel="stylesheet" href="css/plugins.css" />
     <link rel="stylesheet" href="css/style.css" />
 </head>
+<audio loop autoplay>
+<source src="jazzsong_music.mp3" type="audio/mp3">
+</audio>
+
 <body>
     <!-- Preloader -->
     <div class="preloader-bg"></div>
